2387f9f115db8c297b9909d56c42cd947e84d71b
Isaac <bigdataraw@gmail.com>
2016-06-20 11:20:54 -0400

    update

--- read_files.R
+++ read_files.R
@@ -5,3 +5,12 @@ setwd(path)
 hotdogs <- read.delim("hotdogs.txt", header = F, sep = "\t")
 str(hotdogs)
 head(hotdogs)
+#use read.table() to deal with more exotic file formats, header default to F and sep default to ""
+
+# Import the hotdogs.txt file: hotdogs
+path_1 <- file.path("C:/Git/datacamp", "hotdogs.txt")
+
+hotdogs <- read.table(path_1, sep = "\t", col.names = c("type", "calories", "sodium"))
+
+# Call head() on hotdogs
+head(hotdogs)
